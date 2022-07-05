775dd2cd2ac10add424625765e7ae1b59bd1a283
E-Lop <edmondolopez@gmail.com>
2022-07-05 18:16:22 +0200

    bonus terminato

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <MyTitle msg="La mia prima app con Vue CLI!" />
+    <div><MyHero /></div>
   </div>
 </template>
 
 <script>
 import MyTitle from './components/MyTitle.vue';
+import MyHero from './components/MyHero.vue';
 
 export default {
   name: 'App',
   components: {
     MyTitle,
+    MyHero,
   },
 };
 </script>
---
+++ src/components/MyHero.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <img src="../assets/jeeg.jpg" alt="Jeeg e i suoi componenti" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyHero',
+};
+</script>
+
+<style scoped lang="scss">
+img {
+  width: 100%;
+  display: block;
+}
+</style>
